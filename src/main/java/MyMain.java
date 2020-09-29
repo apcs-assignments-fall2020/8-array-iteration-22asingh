7668df36f8b7c7f1fa8dcd09c1779b8c7dbc0532
22asingh <22asingh@athenian.org>
2020-09-29 23:54:07 +0000

    defined methods and tested in main

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,25 +1,58 @@
+import java.util.Arrays;
 public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int[] arr2 = new int[arr.length];
+        for (int i = 0; i < arr.length; i++){
+            arr2[i] = arr[i];
+        }
+        for (int i = 0; i < arr.length; i++){
+            arr2[(arr.length - 1) - i] = arr[i];
+        }
+        return arr2;
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        Arrays.sort(arr);
+        int index = 0;
+        for (int i = arr.length - 2; i >= 0; i--){
+            if (arr[arr.length - 1] > arr[i]){
+                index = i;
+                break;
+            }
+        }
+        return arr[index];
     }
 
     // Checks to see if an array contains a geometric series
     public static boolean isGeometric(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return false;
+        double[] factor = new double[arr.length - 1];
+        for (int i = 0; i < arr.length - 1; i++){
+            factor[i] = Double.valueOf(arr[i+1])/Double.valueOf(arr[i]);
+        }
+        int count = 0;
+        for (int i = 1; i < factor.length; i++){
+            if (factor[i] == factor[i-1]){
+                count += 1;
+            }
+        }
+        if (count == factor.length - 1){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        int[] arr1 = {1,2,3,4};
+        System.out.println(Arrays.toString(reverse(arr1)));
+        int[] arr2 = {42,5,73};
+        System.out.println(secondLargest(arr2));
+        int[] arr3 = {1,2,4,8,16,33};
+        System.out.println(isGeometric(arr3));
     }
 }
